c795975f16718bea967b1398fca2bae1eee53b81
Nandini Sutar <118297471+nandinisutar@users.noreply.github.com>
2024-10-24 12:00:00 +0530

    Add files via upload

---
+++ Datatype/Datatype.java
@@ -0,0 +1,27 @@
+package Datatype;
+
+public class Datatype {
+    public static void main (String args[]){
+    int a = 5;               // integer (whole number)
+    float b = 5.99f;    // floating point number
+    char c = 'D';         // character
+    boolean d = true;       // boolean
+    String e = "Nandini";     // String   
+    byte f = 100;
+    short g = 5000;
+    long h =200000000000L;
+    double i = 26.02d;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    System.out.println(h);
+    System.out.println(i);
+}
+    
+
+}
