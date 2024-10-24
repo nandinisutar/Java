ebad45e49966318781d8427efd2c3f13869152fa
Nandini Sutar <118297471+nandinisutar@users.noreply.github.com>
2024-10-24 12:00:35 +0530

    Add files via upload

---
+++ TypeCasting/Narrow.java
@@ -0,0 +1,11 @@
+package TypeCasting;
+
+public class Narrow {
+    public static void main(String[] args) {
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Explicit casting: double to int
+    
+        System.out.println(myDouble);
+        System.out.println(myInt);
+      }
+}
---
+++ TypeCasting/Widening.java
@@ -0,0 +1,11 @@
+package TypeCasting;
+
+public class Widening {
+    public static void main(String[] args){
+        int myInt=9;
+        double myDouble=myInt; // Automatic casting: int to double
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
