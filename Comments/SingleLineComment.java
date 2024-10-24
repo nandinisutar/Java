f7a39669964ac3dcf4f52122195f16d8bc0a9d19
Nandini Sutar <118297471+nandinisutar@users.noreply.github.com>
2024-10-24 12:00:17 +0530

    Add files via upload

---
+++ Comments/SingleLineComment.java
@@ -0,0 +1,8 @@
+package Comments;
+
+public class SingleLineComment {
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.println("Hello World");
+      }
+}
